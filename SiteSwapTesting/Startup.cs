b116bf3ce746f941db369f21ee2a52171cd40d99
Dave <sanodn@gmail.com>
2017-07-14 09:27:18 +0200

    Write process infomation

---
+++ SiteSwapTesting/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace SiteSwapTesting
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"<b>Current Process</b><br/>Process Name: {Process.GetCurrentProcess().ProcessName}<br/>Process ID: {Process.GetCurrentProcess().Id}<br/>Started: {Process.GetCurrentProcess().StartTime}");
+            });
+        }
+    }
+}
